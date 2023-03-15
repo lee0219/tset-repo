ce25218f4289f1c4b6962408c43649c932520610
lee0798 <71830104+lee0798@users.noreply.github.com>
2023-03-15 16:09:12 +0900

    Update 소스.cpp

--- 소스.cpp
+++ 소스.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main() {
 	printf("Hello test repository");
-	printf("학번 20200239 이름 이도환  컴퓨터공학과");
+	printf("나이: 00 학번 20200239 이름 이도환  컴퓨터공학부");
+	printf("TAB");
 	return 0;
 }
